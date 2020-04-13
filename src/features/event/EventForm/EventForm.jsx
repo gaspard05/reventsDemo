8a7eebb01f5a488ccff4e8ce5a240bc638da0fc9
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2020-04-13 13:04:06 +0100

    Added CRUD operations, end of Section 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -10,9 +10,20 @@ class EventForm extends Component {
     hostedBy: '',
   };
 
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        ...this.props.selectedEvent,
+      });
+    }
+  }
   handleFormSubmit = evt => {
     evt.preventDefault();
-    this.props.createEvent(this.state);
+    if (this.state.id) {
+      this.props.updateEvent(this.state);
+    } else {
+      this.props.createEvent(this.state);
+    }
   };
   handleInputChange = ({ target: { name, value } }) => {
     this.setState({
@@ -21,7 +32,7 @@ class EventForm extends Component {
   };
 
   render() {
-    const { cancelFormOpen} = this.props;
+    const { cancelFormOpen } = this.props;
     const { title, date, city, venue, hostedBy } = this.state;
     return (
       <Segment>
